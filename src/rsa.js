bd877f75db4fcd68aeaa93260fa3b0a8bf7389e8
svitbar <svitbar1102@gmail.com>
2023-06-08 08:36:09 +0300

    Check if number is a prime

--- src/rsa.js
+++ src/rsa.js
@@ -2,4 +2,16 @@ const genRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-console.log(genRandomInt(10, 1000));
+const isPrime = (num) => {
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return num > 1;
+};
+
+const num = genRandomInt(10, 1000);
+console.log(num);
+console.log(isPrime(num));
